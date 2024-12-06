78711a107537f7c9453e3b1e96da148e51f7c0fd
Zach Burns <zachburns1@gmail.com>
2024-12-05 20:20:49 -0700

    just some random improvments to day 5

--- 2024/cmd/day5/day5.go
+++ 2024/cmd/day5/day5.go
@@ -8,27 +8,25 @@ import (
 	"strings"
 )
 
-func correctOrder(order []string, rules_map map[string]map[string]bool) int {
+func ValidateAndGetMiddle(order []string, rules_map map[string]map[string]bool) int {
 	if len(order) == 0 {
 		return 0
 	}
-	match := true
+
 	for i := 0; i < len(order)-1; i++ {
 		if _, ok := rules_map[string(order[i])][string(order[i+1])]; !ok {
-			match = false
+			return 0
 		}
 	}
-	if match {
-		val, _ := strconv.Atoi(string(order[len(order)/2]))
-		return val
-	}
-	return 0
+
+	val, _ := strconv.Atoi(string(order[len(order)/2]))
+	return val
 }
 
 func main() {
 	file := helper.ReadFile("input.txt")
 
-	out := strings.Split(file, "\n\n")
+	out := strings.SplitN(file, "\n\n", 2)
 	rules := out[0]
 	ordering := out[1]
 
@@ -38,40 +36,34 @@ func main() {
 		split_rules := strings.Split(val, "|")
 
 		if _, ok := rules_map[split_rules[0]]; !ok {
-			rules_map[split_rules[0]] = map[string]bool{split_rules[1]: true}
-		} else {
-			holder := rules_map[split_rules[0]]
-			holder[split_rules[1]] = true
-			rules_map[split_rules[0]] = holder
+			rules_map[split_rules[0]] = make(map[string]bool)
 		}
+
+		rules_map[split_rules[0]][split_rules[1]] = true
 	}
 
 	total_1 := 0
 	total_2 := 0
+
 	for _, order := range strings.Split(ordering, "\n") {
 		if len(order) == 0 {
 			continue
 		}
+
 		order := strings.Split(order, ",")
-		val := correctOrder(order, rules_map)
+		val := ValidateAndGetMiddle(order, rules_map)
 
 		if val == 0 {
 			sort.Slice(order, func(i, j int) bool {
-				if rules_map[order[i]][order[j]] {
-					return true
-				} else if rules_map[order[j]][order[i]] {
-					return false
-				} else {
-					return false
-				}
+				return rules_map[order[i]][order[j]]
 			})
-            total_2 += correctOrder(order, rules_map)
+			total_2 += ValidateAndGetMiddle(order, rules_map)
 		} else {
 			total_1 += val
 		}
 
 	}
+
 	fmt.Println("Part 1 Total:", total_1)
 	fmt.Println("Part 2 Total:", total_2)
-
 }
