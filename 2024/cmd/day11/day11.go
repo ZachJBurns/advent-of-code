239313dba87735cb98c3ec09b023913162cb60b7
Zach Burns <zachburns1@gmail.com>
2024-12-11 20:07:53 -0700

    random optimizations

--- 2024/cmd/day11/day11.go
+++ 2024/cmd/day11/day11.go
@@ -7,47 +7,50 @@ import (
 	"strings"
 )
 
+func countStones(freq map[string]int) int {
+	totalStones := 0
+	for _, count := range freq {
+		totalStones += count
+	}
+    return totalStones
+}
+
 func main() {
 	file := helper.ReadFile("input.txt")
 	file = strings.ReplaceAll(file, "\n", "")
 
 	stones := strings.Split(file, " ")
-	puzzle := [][]int{{1, 25}, {2, 75}}
-	for _, round := range puzzle {
-		stoneFrequency := make(map[string]int)
-		for _, stone := range stones {
-			stoneFrequency[stone]++
-		}
-
-		for i := 0; i < round[1]; i++ {
-			updatedFrequency := make(map[string]int)
-			for stone, count := range stoneFrequency {
-				if stone == "0" {
-					updatedFrequency["1"] += count
-				} else if len(stone)%2 == 0 {
-					first := stone[:len(stone)/2]
-					second := stone[len(stone)/2:]
-
-					if len(second) > 1 && second[0] == '0' {
-						stoneInt, _ := strconv.Atoi(second)
-						second = strconv.Itoa(stoneInt)
-					}
-					updatedFrequency[first] += count
-					updatedFrequency[second] += count
-				} else {
-					stoneInt, _ := strconv.Atoi(stone)
-					updatedFrequency[strconv.Itoa(stoneInt*2024)] += count
-				}
-			}
-			stoneFrequency = updatedFrequency
-		}
-
-		totalStones := 0
-		for _, count := range stoneFrequency {
-			totalStones += count
-		}
-
-		fmt.Printf("Part %d Answer: %d\n", round[0], totalStones)
+	stoneFrequency := make(map[string]int)
+	for _, stone := range stones {
+		stoneFrequency[stone]++
 	}
 
+	for i := 0; i < 75; i++ {
+		updatedFrequency := make(map[string]int)
+		for stone, count := range stoneFrequency {
+			if stone == "0" {
+				updatedFrequency["1"] += count
+			} else if len(stone)%2 == 0 {
+				first := stone[:len(stone)/2]
+				second := stone[len(stone)/2:]
+
+				if len(second) > 1 && second[0] == '0' {
+					stoneInt, _ := strconv.Atoi(second)
+					second = strconv.Itoa(stoneInt)
+				}
+				updatedFrequency[first] += count
+				updatedFrequency[second] += count
+			} else {
+				stoneInt, _ := strconv.Atoi(stone)
+				updatedFrequency[strconv.Itoa(stoneInt*2024)] += count
+			}
+		}
+
+		stoneFrequency = updatedFrequency
+		if i == 24 {
+			fmt.Println("Part 1 Answer:", countStones(stoneFrequency))
+		}
+	}
+
+	fmt.Println("Part 2 Answer:", countStones(stoneFrequency))
 }
